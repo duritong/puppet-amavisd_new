55474bd912409ecb2ce2f5392966e1b6b8f1cc21
mh <mh@immerda.ch>
2010-03-30 03:42:57 +0200

    fix path

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -5,7 +5,7 @@ class amavisd-new::centos inherits amavisd-new::base {
   file{'/etc/amavisd/amavisd.conf':
     source => [ "puppet://$server/modules/site-amavisd-new/${fqdn}/amavisd.conf",
                 "puppet://$server/modules/site-amavisd-new/amavisd.conf",
-                "puppet://$server/modules/site-amavisd-new/amavisd.conf" ],
+                "puppet://$server/modules/amavisd-new/amavisd.conf" ],
     require => Package['amavisd-new'],
     owner => root, group => 0, mode => 0644;
   }
