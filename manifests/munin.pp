30c56a3d0f56b4e8828fa44a7919a27873b6cf69
mh <mh@immerda.ch>
2010-03-31 04:16:27 +0200

    minor cleanup

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -8,10 +8,10 @@ class amavisd-new::munin {
     centos: { $munin_amavis_db_location = '/var/spool/amavisd/db/' }
     default: { $munin_amavis_db_location = '/var/lib/amavis/db' }
   }
+
   munin::plugin{ [ 'amavis_state', 'amavis_content', 'amavis_time' ]:
     require => Munin::Plugin::Deploy['amavis_'],
     ensure => 'amavis_',
     config => "env.amavis_db_home ${munin_amavis_db_location}\nuser amavis",
   }
-
 }
