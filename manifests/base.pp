8c7b4841ee7c9ea28138f9d9bab058fe14d30ad6
Peter Meier <peter.meier@immerda.ch>
2010-03-31 12:37:23 +0200

    add unzip extracter to be installed

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,12 @@
 class amavisd-new::base {
   #unrar packages for amavis
-  package{ [ 'arc', 'cabextract', 'freeze', 'unrar', 'lha', 'zoo', 'unarj' ]:
+  package{ [ 'arc', 'cabextract', 'freeze', 'unrar', 'lha', 'zoo', 'unarj', 'unzip' ]:
     ensure => installed,
   }
 
   package{'amavisd-new':
     ensure => installed,
-    require => Package['arc', 'cabextract', 'freeze', 'unrar', 'lha', 'zoo', 'unarj'],
+    require => Package['arc', 'cabextract', 'freeze', 'unrar', 'lha', 'zoo', 'unarj', 'unzip' ],
   }
 
   service{'amavisd':
