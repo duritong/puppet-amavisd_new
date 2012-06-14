347d2d72caf299d66078db89b61dc68d52aa3879
o <o@immerda.ch>
2012-06-14 11:17:11 -0300

    remove hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,17 +4,18 @@
 # this module is part of a whole bunch of modules, please have a look at the exim module
 
 class amavisd_new(
-  $viruscheck = hiera('amavis_viruscheck',false),
-  $spamcheck = hiera('amavis_spamcheck',false)
+  $viruscheck   = false,
+  $spamcheck    = false,
+  $manage_munin = false
 ) {
   case $::operatingsystem {
     gentoo: { include amavisd_new::gentoo }
     centos: { include amavisd_new::centos }
-    debian,ubuntu: { include amavisd-new::debian }
+    debian,ubuntu: { include amavisd_new::debian }
     default: { include amavisd_new::base }
   }
 
-  if hiera('use_munin',false) {
+  if $manage_munin {
     include amavisd_new::munin
   }
 }
