a4ac55194d98e4a98702b375131dabc4adff1110
mh <mh@immerda.ch>
2012-06-18 23:46:09 -0300

    fix source

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -3,9 +3,9 @@ class amavisd_new::centos inherits amavisd_new::base {
     name => 'arj',
   }
   file{'/etc/amavisd/amavisd.conf':
-    source => [ "puppet:///modules/site_amavisd-new/${::fqdn}/amavisd.conf",
-                "puppet:///modules/site_amavisd-new/amavisd.conf",
-                "puppet:///modules/amavisd-new/amavisd.conf" ],
+    source => [ "puppet:///modules/site_amavisd_new/${::fqdn}/amavisd.conf",
+                "puppet:///modules/site_amavisd_new/amavisd.conf",
+                "puppet:///modules/amavisd_new/amavisd.conf" ],
     require => Package['amavisd-new'],
     notify => Service['amavisd'],
     owner => root, group => 0, mode => 0644;
