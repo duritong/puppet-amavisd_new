889d06b9f175ef0907626a3f26efceabf2027962
mh <mh@immerda.ch>
2013-09-26 15:12:20 +0200

    amavisd's cache support has been dropped since amavisd-new 2.7

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,7 +1,8 @@
+# manage amavisd munin plugins
 class amavisd_new::munin {
   munin::plugin::deploy{'amavis_':
-    source => "amavisd_new/munin/amavis_",
     ensure => absent,
+    source => 'amavisd_new/munin/amavis_',
   }
 
   case $::operatingsystem {
@@ -9,9 +10,17 @@ class amavisd_new::munin {
     default: { $munin_amavis_db_location = '/var/lib/amavis/db' }
   }
 
-  munin::plugin{ [ 'amavis_cache', 'amavis_content', 'amavis_time' ]:
+  munin::plugin{ [ 'amavis_content', 'amavis_time' ]:
+    ensure  => 'amavis_',
     require => Munin::Plugin::Deploy['amavis_'],
-    ensure => 'amavis_',
-    config => "env.amavis_db_home ${munin_amavis_db_location}\nuser amavis",
+    config  => "env.amavis_db_home ${munin_amavis_db_location}\nuser amavis",
+  }
+  # this feature is only available in versions available on that system
+  if ($::operatingsystem == 'CentOS') and ($::operatingsystemmajrelease < 6)
+    munin::plugin{'amavis_cache':
+      ensure  => 'amavis_',
+      require => Munin::Plugin::Deploy['amavis_'],
+      config  => "env.amavis_db_home ${munin_amavis_db_location}\nuser amavis",
+    }
   }
 }
