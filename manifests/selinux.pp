cac3add208cb725b67ebe6041977901c91d03457
mh <mh@immerda.ch>
2015-04-07 23:11:39 +0200

    allow scan on all systems

--- manifests/selinux.pp
+++ manifests/selinux.pp
@@ -1,11 +1,9 @@
 # amavisd/antivir should be able to scan
 # the system
 class amavisd_new::selinux {
-  if ($::osfamily == 'RedHat') and (versioncmp($::operatingsystemmajrelease,'7') < 0) {
-    selboolean{
-      'antivirus_can_scan_system':
-        value      => 'on',
-        persistent => true,
-    }
+  selboolean{
+    'antivirus_can_scan_system':
+      value      => 'on',
+      persistent => true,
   }
 }
