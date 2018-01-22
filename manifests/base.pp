744515acf4cf0b155fabb1f293fff523a80c508a
mh <mh@immerda.ch>
2018-01-22 20:06:06 +0100

    add another package for amavisd

--- manifests/base.pp
+++ manifests/base.pp
@@ -3,11 +3,11 @@ class amavisd_new::base {
   #unrar packages for amavis
   require unzip
   require unrar
-  ensure_packages([ 'arc', 'cabextract', 'freeze', 'lha', 'zoo', 'unarj', ])
+  ensure_packages([ 'arc', 'cabextract', 'freeze', 'lha', 'zoo', 'unarj', 'lz4'])
 
   package{'amavisd-new':
     ensure  => installed,
-    require => Package['arc', 'cabextract', 'freeze', 'lha', 'zoo', 'unarj' ],
+    require => Package['arc', 'cabextract', 'freeze', 'lha', 'zoo', 'unarj', 'lz4'],
   }
 
   service{'amavisd':
